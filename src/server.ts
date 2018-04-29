f175eb5c50991884ad538c036f7295c06f5492bc
Baron Chen <baron.zhongyangchen@gmail.com>
2018-04-29 21:53:54 +1000

    Get pupeteer to wok with docker

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,38 @@
 import * as Koa from 'koa';
 import { Context } from 'koa';
+import * as Router from 'koa-router';
+import { Url2PdfController } from './url-2-pdf/url-2-pdf-controller';
 
 export class MyApp {
 
   app: Koa;
+  router: Router;
 
   constructor() {
     this.app = new Koa();
+    this.router = new Router();
   }
 
   start() {
-    this.app.listen(3000);
     this.registerActions();
+    this.app.use(this.router.routes()).use(this.router.allowedMethods());
 
+    this.app.listen(3000);
     console.log("server listenning to port 3000...");
   }
 
   registerActions() {
-    this.app.use(async (ctx: Context) => {
-      ctx.body = 'Hello lalal';
-    });
+
+    this.router.get('/', async (ctx: Context) => {
+      ctx.body = 'Hello url2pdf!';
+    })
+    .get('/url2pdf/convert', async (ctx: Context) => {
+      const url = decodeURIComponent(ctx.request.query['url']);
+      let controller = new Url2PdfController();
+      ctx.response.set('Content-disposition', 'attachment; filename=converted.pdf');
+      ctx.response.set('Content-type', 'application/pdf');
+      ctx.body = await controller.convert(url);
+    });    
   }
 
 }
---
+++ src/url-2-pdf/url-2-pdf-controller.ts
@@ -0,0 +1,18 @@
+import * as puppeteer from 'puppeteer';
+
+export class Url2PdfController {
+
+  constructor() {
+  }
+
+  async convert(url): Promise<Buffer>{
+    const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-dev-shm-usage', '--disable-setuid-sandbox']});
+    const page = await browser.newPage();
+    await page.goto(url, { waitUntil: 'networkidle2' });
+    await page.emulateMedia('screen');
+    let buffer = await page.pdf({ format: 'A4' });
+    await browser.close();
+    return buffer;
+  }
+
+}
